2d574f611a14e84c02ee6a37d76998ee567b4b8c
edwin463 <eliudedwin5@gmail.com>
2025-02-19 15:52:09 +0300

    practice on flaskapp

--- server/app.py
+++ server/app.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)  # Print to the console
+    return text  # Display in the browser
+
+@app.route('/count/<int:num>')
+def count(num):
+    count_str = "\n".join(str(i) for i in range(num)) + "\n"
+    return Response(count_str, content_type="text/plain")
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2 if num2 != 0 else 'Error: Division by zero'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+    
+    return str(result)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
